765509b1834f1cc9c2e8e68c16fbf10a0f89f595
Roeen <roeenacal@gmail.com>
2023-01-28 16:02:07 -0800

    Add hamburger html, css, and separate js

---
+++ assets/js/hamburger.js
@@ -0,0 +1,15 @@
+// Hamburger 
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active')
+	navMenu.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-item').forEach(item => item.addEventListener('click', () => {
+    navMenu.classList.remove('active')
+    hamburger.classList.remove('active')
+
+}))
